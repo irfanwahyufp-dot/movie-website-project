032c6ebed119a951554ad76dc5387ecc4d1ab3f0
IrfanWahyuFadhilaPutra <irfanwahyufp@gmail.com>
2026-03-12 07:23:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("FannFlix Loaded");
+console.log("FannFlix loaded!");
+
+const movies = document.querySelectorAll(".movie-card");
+
+movies.forEach(movie => {
+    movie.addEventListener("click", function() {
+        const title = this.querySelector("h3").innerText;
+        alert("You selected: " + title);
+    });
+});
